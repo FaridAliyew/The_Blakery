e168a4ed7b090f8b00ebd926ee9d0fea8a387678
FaridAliyev <feliyev636@gmail.com>
2025-03-08 17:03:43 +0400

    added Header and Navbar component

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import './style/style.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <section id='header-component'>
+      <h1 className='text-center text-white text-[1.029rem] uppercase bg-[rgb(50,47,48)]'>15% OFF USING CODE "LOVE15" + free shipping on $60+</h1>
+    </section>
+  )
+}
+
+export default Header
